0ab17b5d14fbfba927af8f544bafd997297b07c4
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-04-09 18:36:41 -0500

    val y var

--- src/app/Primer.kt
+++ src/app/Primer.kt
@@ -1,3 +1,9 @@
 fun main() {
     println("Holis")
+    val name: String = "Edo"
+    var name2: String = "Mcs"
+    println(name + name2)
+    // name = "no" <- Esto es ilegal bruh
+    name2 = "Cs"
+    println(name + name2)
 }
