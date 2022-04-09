7abc5a75d986e95271654c8a5d7284c5e702dbe1
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-04-09 18:54:33 -0500

    comentarios bonitos

--- src/app/Primer.kt
+++ src/app/Primer.kt
@@ -3,14 +3,19 @@ fun main() {
     val name: String = "Edo"
     var name2: String = "Mcs"
     println(name + name2)
-    // name = "no" <- Esto es ilegal bruh
+    // name = "no" <- Esto es ilegal bruh, variables val no pueden
+    // ser modifivadas
     name2 = "Cs"
     println(name + name2)
     print("Hi")
     println(name)
+    // Kotlin infiere los tipos de datos de las variables, sin embargo
+    // serán variables no-null pues es lo predeterminado
     val edad = 24
     var contador = 1
     println(edad + contador)
+    // Para que una variable sea nullable debe declarase su tipo
+    // y agregarse un ?
     val anio: Int? = null
     println(anio)
 }
