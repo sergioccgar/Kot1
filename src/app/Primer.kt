b5cc3565f4fbad4ecba67db196f1474d013fff01
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-04-09 19:15:23 -0500

    do-while, when, incremento

--- src/app/Primer.kt
+++ src/app/Primer.kt
@@ -13,7 +13,8 @@ fun main() {
     // serán variables no-null pues es lo predeterminado
     val edad = 24
     var contador = 1
-    println(edad + contador)
+    println(edad + contador) // Se imprime la suma de los valores
+    println("$edad" + "$contador") // Se imprime la concatenación en String de los valores
     // Para que una variable sea nullable debe declarase su tipo
     // y agregarse un ?
     var anio: Int? = null
@@ -24,4 +25,14 @@ fun main() {
     } else {
         println("NO HAY AÑO AAAAAAAAAAAAAAH")
     }
+    // do-while, when, incremento
+    do {
+        when (contador) {
+            1 -> println("uno")
+            2 -> println("dos")
+            3 -> println("tres")
+            else -> println("nono")
+        }
+        contador++
+    } while (contador < 10)
 }
